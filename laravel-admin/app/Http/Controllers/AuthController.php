278c144d9f4e2362d3702ab75c90265c2cde8f04
NozomuTsuruta <tuno.0806@icloud.com>
2020-10-18 16:21:32 +0900

    うまく登録できないのを修正

--- laravel-admin/app/Http/Controllers/AuthController.php
+++ laravel-admin/app/Http/Controllers/AuthController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\RegisterRequest;
-use Hash;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
@@ -30,7 +31,7 @@ class AuthController extends Controller
     public function register(RegisterRequest $request)
     {
         $user = User::create(
-            $request->only('first_name', 'last_name', 'email', 'password')
+            $request->only('first_name', 'last_name', 'email')
             + ['password'=>Hash::make($request->input('password'))]
         );
 
