23ef0bfa007b63072e967be721281f3fcf500e5e
NozomuTsuruta <tuno.0806@icloud.com>
2020-10-20 14:09:46 +0900

    画像アップロード

--- laravel-admin/app/Http/Controllers/ProductController.php
+++ laravel-admin/app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
@@ -23,7 +24,18 @@ class ProductController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $file = $request->file('image');
+        $name = Str::random(10);
+        $url = \Storage::putFileAs('images', $file, $name.'.'.$file->extension());
+
+        $product = Product::create([
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'image'=>env('APP_URL').'/'.$url,
+            'price'=>$request->input('price'),
+        ]);
+
+        return response($product, Response::HTTP_CREATED);
     }
 
     public function update(Request $request, $id)
