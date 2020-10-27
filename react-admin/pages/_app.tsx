65659e9dab2ba46b6e5715e964a5eb982f916a04
NozomuTsuruta <tuno.0806@icloud.com>
2020-10-28 08:42:13 +0900

    front,registerまで作成

---
+++ react-admin/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { AppProps } from 'next/app';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/App.css';
+import { Nav } from '../components/Nav';
+import { Menu } from '../components/Menu';
+
+export default function App({ Component, pageProps }: AppProps) {
+    return (
+        <div className="App">
+            <Nav />
+            <div className="container-fluid">
+                <div className="row">
+                    <Menu />
+                    <main
+                        role="main"
+                        className="col-md-9 ml-sm-auto col-lg-10 px-md-4"
+                    >
+                        <Component {...pageProps} />{' '}
+                    </main>
+                </div>
+            </div>
+        </div>
+    );
+}
